5cc628202221d870bbd3d62f6e2e412b87c8ea83
Shani Shlapobersky <shani@kepempem.com>
2016-08-18 09:24:06 +0300

    Create jsbn.js

---
+++ src/jsbn.js
@@ -0,0 +1,64 @@
+/* - JSBN JS Handler - v1.0 - Beta - By Shani Shlapobersky - Licensed under the MIT license - */
+var bots = [];
+function bot(name) {
+  if (this === window) {
+    return new bot(name);
+  } else {
+    this.name = name;
+    this.loadSource = function (src) {
+      if (src.substring(src.length - 5, src.length) !== ".jsbn") {
+      throw new Error("Bot Source must be a JSBN file.");
+    } else {
+    sendAJAXRequest(src, function (request) {
+      if (JSON.parse) {
+        var JSBNC = JSON.parse(request.responseText);
+      } else {
+        var JSBNC = eval ("(" + request.responseText + ")");
+      }
+      if (JSBNC['title'] !== this.name) {
+        throw new Error("Bot name must be identical to the property 'title' in your bot's JSBN file.");
+      } else {
+        this.ready = true;
+        var details = "Bot " + JSBNC['title'] + "is built";
+        if (JSBNC['version']) {
+          details += " and running version " + JSBNC['version'].toString();
+        }
+        if (JSBNC['author']) {
+          details += ".\n\n\nBot by " + JSBNC['author'];
+        }
+        details += ".";
+        this.send = function(input) {
+          //Define Send function
+        }
+    });
+  }
+}
+var CrossBrowserAjaxObjects = [
+    function () {return new XMLHttpRequest()},
+    function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+    function () {return new ActiveXObject("Msxml3.XMLHTTP")},
+    function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+];
+function RequestObject() {
+    var xmlhttp = false;
+    for (var ObjectCounter = 0; ObjectCounter < CrossBrowserAjaxObjects.length; ObjectCounter++) {
+        try {
+            xmlhttp = CrossBrowserAjaxObjects[ObjectCounter]();
+        }
+        catch (e) {
+            continue;
+        }
+        break;
+    }
+    return xmlhttp;
+};
+function sendAJAXRequest(URL, callback) {
+  var xhttp = RequestObject();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      callback(xhttp);
+    }
+  };
+  xhttp.open("GET", URL, true);
+  xhttp.send();
+};
